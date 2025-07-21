f0de39221a233caa33afcbad75fefb4dd85fbba1
Mutonya <georgekmutonya@gmail.com>
2025-07-21 14:36:04 +0300

    render

--- src/main/java/com/mutonya/hmisapplication/auth/controller/AuthController.java
+++ src/main/java/com/mutonya/hmisapplication/auth/controller/AuthController.java
@@ -11,10 +11,7 @@ import com.mutonya.hmisapplication.auth.utils.apiresponse.ApiResponse;
 import com.mutonya.hmisapplication.auth.utils.apiresponse.ResponseBuilder;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("api/v1/auth")
@@ -27,7 +24,10 @@ public class AuthController {
         this.authService = authService;
     }
 
-
+    @GetMapping("/test")
+    public ResponseEntity<?> getSensitiveData() {
+        return ResponseEntity.ok("You have access");
+    }
     @PostMapping("/register")
 
     public  ResponseEntity<ApiResponse<RegisterResponse>> registerUser(
